770a29d78e40c79656245a81a51079f3bd460bbd
Sandis <agentsandis@gmail.com>
2021-06-18 13:30:09 +0300

    This is main commit v1.0

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass");
+const wait = require("gulp-wait");
+const rename = require("gulp-rename");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
+
+// Sass Task
+function scssTask() {
+  return (
+    src(["src/scss/main.scss"], {
+      sourcemaps: true,
+    })
+      .pipe(wait(300))
+      .pipe(sass())
+      //.pipe(postcss([cssnano()]))
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(dest("public/css/", { sourcemaps: "." }))
+  );
+}
+
+// JavaScript Task
+function jsTask() {
+  return src("src/js/scripts.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("public/js/", { sourcemaps: "." }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb) {
+  browsersync.init({
+    //logLevel: 'debug',
+    open: "external",
+    host: "git.localhost",
+    proxy: "git.localhost",
+    port: 8080,
+  });
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch("./**/*.php", browsersyncReload);
+  watch("./**/*.html", browsersyncReload);
+  watch(
+    ["src/**/*.scss", "src/**/*.js", "public/images/*.*"],
+    series(scssTask, jsTask, browsersyncReload)
+  );
+}
+
+// Default Gulp task
+exports.default = series(
+  //scssTask,
+  //jsTask,
+  browsersyncServe,
+  watchTask
+);
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Basic Project Template</title>
+    <link rel="stylesheet" href="public/css/main.min.css?v=<?php echo rand(0, 1000000) ?>">
+
+</head>
+
+<body>
+    <h1><?php echo "Hello, World!"; ?></h1>
+    <div class="notes">
+        <p>Starting a new project:</p>
+        <ul>
+            <li>Make new project directory <code>mkdir "new-project"</code></li>
+            <li>Run <code>git clone dfynest/new-project--basic</code> to copy template from github</li>
+            <li>Run <code>npm status</code> to check if npm installed</li>
+            <li>Run <code>npm install</code> to install all packages from <em>packages.json</em> file</li>
+            <li>Edit <code>gulpfile.js</code> and change virtual host record (eg git.localhost)</li>
+            <li>Run <code>gulp</code></li>
+        </ul>
+    </div>
+    <script src="public/js/scripts.min.js"></script>
+</body>
+
+</html>
